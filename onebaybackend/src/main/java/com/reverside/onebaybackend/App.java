5ba49b7f82974ab4bff59b5ac66d481491ddfaab
Pride116 <Pridetumisho116@gmail.com>
2020-01-10 09:51:42 +0200

    My project setup completed

---
+++ onebaybackend/src/main/java/com/reverside/onebaybackend/App.java
@@ -0,0 +1,13 @@
+package com.reverside.onebaybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
